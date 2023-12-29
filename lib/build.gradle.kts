8c6816f631b6dbb278d48c6ef1644790d833f2d3
yevhen <yevhen.railian.v@gmail.com>
2023-12-30 01:21:26 +0200

    downgrade jvm version from 17 to 1.8

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -1,4 +1,5 @@
 import org.jetbrains.kotlin.gradle.dsl.ExplicitApiMode
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
     alias(libs.plugins.kotlin.jvm)
@@ -6,6 +7,14 @@ plugins {
 
 kotlin {
     explicitApi = ExplicitApiMode.Strict
+    compilerOptions {
+        jvmTarget = JvmTarget.JVM_1_8
+    }
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
 }
 
 dependencies {
