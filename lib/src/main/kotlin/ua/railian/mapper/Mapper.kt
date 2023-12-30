d7b706b5bb398d1659a139043c35751bc98f6a94
yevhen <yevhen.railian.v@gmail.com>
2023-12-30 12:21:47 +0200

    add kotlin documentation

--- lib/src/main/kotlin/ua/railian/mapper/Mapper.kt
+++ lib/src/main/kotlin/ua/railian/mapper/Mapper.kt
@@ -1,5 +1,18 @@
 package ua.railian.mapper
 
+/**
+ * Simple mapper to map object from one type to another.
+ *
+ * @param T source object type
+ * @param R destination object type
+ */
 public fun interface Mapper<T, R> {
+
+    /**
+     * Map object from one type to another.
+     *
+     * @param source object to map
+     * @return mapped object
+     */
     public fun map(source: T): R
 }
--- lib/src/main/kotlin/ua/railian/mapper/TwoWayMapper.kt
+++ lib/src/main/kotlin/ua/railian/mapper/TwoWayMapper.kt
@@ -1,12 +1,49 @@
 package ua.railian.mapper
 
+/**
+ * Mapper that can map both directions:
+ * forward - from [T] to [R] and backward - from [R] to [T].
+ */
 public interface TwoWayMapper<T, R> {
+
+    /**
+     * Mapper to map object in forward direction from [T] to [R].
+     *
+     * @see Mapper
+     */
     public val forward: Mapper<T, R>
+
+    /**
+     * Mapper to map object in backward direction from [R] to [T].
+     *
+     * @see Mapper
+     */
     public val backward: Mapper<R, T>
+
+    /**
+     * Map object in forward direction from [T] to [R].
+     *
+     * @param source object to map
+     * @return mapped object
+     */
     public fun mapForward(source: T): R = forward.map(source)
+
+    /**
+     * Map object in backward direction from [R] to [T].
+     *
+     * @param source object to map
+     * @return mapped object
+     */
     public fun mapBackward(source: R): T = backward.map(source)
 }
 
+/**
+ * Create [TwoWayMapper] from two simple [Mapper]s.
+ *
+ * @param forward mapper to map object from [T] to [R]
+ * @param backward mapper to map object from [R] to [T]
+ * @return mapper that can map both directions
+ */
 public fun <T, R> twoWayMapper(
     forward: Mapper<T, R>,
     backward: Mapper<R, T>,
